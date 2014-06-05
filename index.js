feb4aa49de3f2d9f010e11a230840d1298f27b91
Nadim <nadim.boustani@gmail.com>
2014-06-05 14:39:56 +0200

    improve mongo connection parameters

--- index.js
+++ index.js
@@ -2,35 +2,21 @@ var hubiquitus = require('hubiquitus-core');
 var mongo = require('mongodb');
 var _ = require('lodash');
 var util = require('util');
+var MongoClient = require('mongodb').MongoClient;
 
 var db = null;
 var conf = {};
 
 exports.configure = function (properties, done) {
-  conf.host = properties.host || '127.0.0.1';
-  conf.port = properties.port || mongo.Connection.DEFAULT_PORT;
-  conf.dbname = properties.dbname || 'hubiquitus';
+  conf.mongo = properties.mongo || 'mongodb://localhost:27017/logs';
   conf.collection = properties.collection || 'logs';
-  conf.username = properties.username;
-  conf.password = properties.password;
 
-  var server = new mongo.Server(conf.host, conf.port, {auto_reconnect: true});
-  var client = new mongo.MongoClient(server);
-  client.open(function (err, client) {
+  MongoClient.connect(properties.mongo, {mongos: {'auto_reconnect': true}} , function(err, _db) {
     if (err) return (done && done(err));
-    db = client.db(conf.dbname);
+    db = _db;
 
-    if (conf.username) {
-     db.authenticate(conf.username, conf.password, function (err, result) {
-       if (err) return (done && done(err));
-       if (result != true) return (done && done(new Error('authentication error')));
-       overrideLogger();
-       done && done();
-     });
-    } else {
-      overrideLogger();
-      done && done();
-    }
+    overrideLogger();
+    done && done();
   });
 };
 
